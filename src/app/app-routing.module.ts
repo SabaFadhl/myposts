322ae6c4ee4358f93e9f5062b96b8602ec5f9c50
SabaFadhl <sabafadl98@gmail.com>
2022-07-19 11:33:48 +0300

    routing and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { CreateComponent } from './posts/create/create.component';
+import { PostsComponent } from './posts/posts.component';
+import { ShowComponent } from './posts/show/show.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'posts',
+    children:[
+      {
+        path:'',
+        component:PostsComponent
+      },
+      {
+        path:'create',
+        component:CreateComponent
+      },
+      {
+        path:'show',
+        component:ShowComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
